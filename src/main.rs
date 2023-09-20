2384e9cbb3a03adefdeb5e5bd23715580df6dea5
Atilio Pereira <apereira@consumeraffairs.com>
2023-09-20 13:41:57 -0400

    Read incoming TCP connections

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},  
+};
+
+fn handle_client(mut stream: TcpStream) {
+    let address = stream.local_addr().unwrap();
+    println!("Handling connection from: {}", address);
+
+    let mut data:[u8; 2048] = [0; 2048];
+    let byte_count = stream.read(&mut data).unwrap();
+    let data_string = String::from_utf8_lossy(&data);
+
+    println!("Data: {:?}", &data[..byte_count]);
+    println!("\nData string:\n{}\n", &data_string);
+}
+
+fn main() -> std::io::Result<()> {
+    let listner = TcpListener::bind("0.0.0.0:11111")?;
+    for stream in listner.incoming() {
+        handle_client(stream?);
+    }
+    println!("Program ended!");
+    Ok(())
+}
